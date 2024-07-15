3bf6b3db9c24d4dba74dae3b7bc7a515cd570246
Sor4chi <80559385+sor4chi@users.noreply.github.com>
2024-07-15 16:19:59 +0900

    test: add multiline usecrate case

--- tests/testdata/input/usecrate/src/main.rs
+++ tests/testdata/input/usecrate/src/main.rs
@@ -1,6 +1,8 @@
 extern crate usecrate;
 use usecrate::submod2::hello_world2;
+use usecrate::submod4::hello_world4;
 
 fn main() {
     hello_world2();
+    hello_world4();
 }
---
+++ tests/testdata/input/usecrate/src/submod3.rs
@@ -0,0 +1,3 @@
+pub fn hello_world3__long_identifier_name_for_multiline() {
+    println!("Hello, world 3!");
+}
